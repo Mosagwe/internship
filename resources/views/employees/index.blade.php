18fa56c231ee77ea3eadf0610dbff0ca52354772
Mosagwe <mospeto@gmail.com>
2021-07-28 08:02:21 +0300

    Add url import employee

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 @section('title','Employees')
 @section('action')
-    <a href="{{ route('employee.create') }}" class="float-right mr-3 btn btn-sm btn-success shadow-sm d-none d-sm-inline-block">
-        <i class="fas fa-plus-circle fa-sm text-white-50"></i>
-        Create New
-    </a>
+    <div>
+        <a href="{{ route('employee.create') }}" class="float-right mr-3 btn btn-sm btn-success shadow-sm d-none d-sm-inline-block">
+            <i class="fas fa-plus-circle fa-sm text-white-50"></i>
+            Create New
+        </a>
+        <a href="{{ route('employeesimport.create') }}" class="float-right mr-3 btn btn-sm btn-success shadow-sm d-none d-sm-inline-block">
+            <i class="fas fa-plus-circle fa-sm text-white-50"></i>
+            Import Employees
+        </a>
+    </div>
+
+
 @endsection
 @section('content')
         <div class="col-md-12 mt-1 mx-auto">
