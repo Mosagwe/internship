308a5be2b322632c9454eb214a08d3297216f747
Mosagwe <mospeto@gmail.com>
2021-07-30 07:16:43 +0300

    Add Edit contract

--- resources/views/contracts/action.blade.php
+++ resources/views/contracts/action.blade.php
@@ -23,33 +23,62 @@
     </div>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="editContractModal" tabindex="-1" role="dialog" aria-labelledby="editContractModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editContractModalLabel">Edit Contract</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <form action="">
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="fullname">Full Name</label>
-                        <input type="text" class="form-control" id="" value="{{ $contract->employee->fullname }}">
-                    </div>
 
+        <!-- Modal -->
+<div class="modal fade justify-content-between" id="editContractModal" tabindex="-1" role="dialog" aria-labelledby="editContractModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editContractModalLabel">Edit Contract</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action="">
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="modal-body jsgrid-align-left">
+                                    <div class="form-group">
+                                        <label for="fullname">Full Name</label>
+                                        {{ strtoupper($contract->employee->fullname) }}
+                                    </div>
+                                    <hr>
+                                    <div class="form-group">
+                                        <label>Start Date</label>
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                                            <input type="text"
+                                                   class="form-control datepicker @error('date_hired') is-invalid @enderror"
+                                                   id="date_hired" name="date_hired"
+                                                   value="{{ $contract->start_date }}"
+                                                   autocomplete="off">
+                                            @error('date_hired')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="formGroupExampleInput2">Station</label>
+                                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                                    </div>
+                                </div>
+                                <div class="modal-footer justify-content-between">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-success">Update</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </form>
+            </div>
         </div>
-    </div>
-</div>
+
+
+
 
 
 
