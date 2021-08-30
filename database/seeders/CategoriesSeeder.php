7d5fbba255d1bfd63fb65a51b726df0900721b48
Mosagwe <mospeto@gmail.com>
2021-08-30 12:05:14 +0300

    Rename Categories seeder

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -15,25 +15,25 @@ class CategoriesSeeder extends Seeder
     public function run()
     {
         $categories=[
-            ['id'=>1,'name'=>'Centre Assistant','parent_id'=>null,'salary'=>null],
-            ['id'=>2,'name'=>'Centre Assistant - Tea','parent_id'=>1,'salary'=>13000],
-            ['id'=>3,'name'=>'Centre Assistant - Tea II','parent_id'=>1,'salary'=>20000],
-            ['id'=>4,'name'=>'Centre Assistant - Cleaner','parent_id'=>1,'salary'=>13000],
+            ['id'=>1,'name'=>'Hospitality','parent_id'=>null,'salary'=>null],
+            ['id'=>2,'name'=>'Hospitality Assistant II','parent_id'=>1,'salary'=>13000],
+            ['id'=>3,'name'=>'Hospitality Assistant  I','parent_id'=>1,'salary'=>20000],
+            ['id'=>4,'name'=>'Support Staff','parent_id'=>null,'salary'=>13000],
 
             ['id'=>5,'name'=>'Security','parent_id'=>null,'salary'=>null],
-            ['id'=>6,'name'=>'X-NYS','parent_id'=>5,'salary'=>20000],
+            ['id'=>6,'name'=>'Security Wardens','parent_id'=>5,'salary'=>20000],
             ['id'=>7,'name'=>'NYS','parent_id'=>5,'salary'=>null],
 
-            ['id'=>8,'name'=>'Contact Center','parent_id'=>null,'salary'=>25000],
+            ['id'=>8,'name'=>'Contact Center Agent','parent_id'=>null,'salary'=>25000],
 
             ['id'=>9,'name'=>'ICT Assistants','parent_id'=>null,'salary'=>null],
-            ['id'=>10,'name'=>'ICT Assistants I','parent_id'=>9,'salary'=>25000],
-            ['id'=>11,'name'=>'ICT Assistants II','parent_id'=>9,'salary'=>20000],
+            ['id'=>10,'name'=>'ICT Assistant I','parent_id'=>9,'salary'=>25000],
+            ['id'=>11,'name'=>'ICT Assistant II','parent_id'=>9,'salary'=>20000],
 
-            ['id'=>12,'name'=>'General Duties','parent_id'=>null,'salary'=>null],
-            ['id'=>13,'name'=>'General Duties I','parent_id'=>12,'salary'=>25000],
-            ['id'=>14,'name'=>'General Duties II','parent_id'=>12,'salary'=>20000],
-            ['id'=>15,'name'=>'General Duties III','parent_id'=>12,'salary'=>13000],
+            ['id'=>12,'name'=>'Customer Care','parent_id'=>null,'salary'=>null],
+            ['id'=>13,'name'=>'Customer Care Assistant I','parent_id'=>12,'salary'=>25000],
+            ['id'=>14,'name'=>'Customer Care Assistant II','parent_id'=>12,'salary'=>20000],
+            ['id'=>15,'name'=>'Customer Care Assistant III','parent_id'=>12,'salary'=>13000],
         ];
 
         foreach ($categories as $category){
